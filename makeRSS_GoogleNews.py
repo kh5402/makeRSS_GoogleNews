c907be57a83696ee4475b0f457c5f6a883d039a6
kh <127534515+kh5402@users.noreply.github.com>
2023-10-17 12:33:19 +0900

    Update makeRSS_GoogleNews.py

--- makeRSS_GoogleNews.py
+++ makeRSS_GoogleNews.py
@@ -1,9 +1,9 @@
 import requests
-import xml.etree.ElementTree as ET
+from lxml import etree
 
 url_and_xmls = [
     {
-        'url': 'https://news.google.com/rss/articles/CBMiNGh0dHBzOi8vd3d3Lm5vZ2l6YWthNDYuY29tL3MvbjQ2L2RpYXJ5L2RldGFpbC8xMDE5ODHSAQA?oc=5',
+        'url': 'https://news.google.com/rss/topics/CAAqKAgKIiJDQkFTRXdvTkwyY3ZNVEZtZERCc2NIaDZNaElDYW1Fb0FBUAE?hl=ja&gl=JP&ceid=JP%3Aja',
         'xml': 'feed_GoogleNews_Yumiki.xml'
     }
 ]
@@ -17,15 +17,15 @@ def fetch_and_save_feeds(url_and_xmls):
         print(f'Fetching RSS feed from {url}')
         response = requests.get(url)
         response.raise_for_status()  # ステータスコードが200 OKでない場合は、エラーを発生させる
-
+        
         # RSSフィードの内容を解析
-        root = ET.fromstring(response.content)
-        entries = root.findall('.//item')
+        root = etree.fromstring(response.content)
+        entries = root.xpath('//item')
         print(f'Fetched {len(entries)} entries.')
 
         # XMLファイルに保存
         print(f'Saving to {xml_filename}')
-        xml_str = ET.tostring(root, encoding='utf-8', method='xml')
+        xml_str = etree.tostring(root, encoding='utf-8', pretty_print=True)
         with open(xml_filename, 'wb') as f:
             f.write(xml_str)
         print(f'Saved to {xml_filename}')
