5aca2d31903a1f5c904e99d2917a10ca60a8d308
kh5402 <kh5402@users.noreply.github.com>
2023-10-17 03:34:22 +0000

    deploy: fa0079a5a9e894a435dff959b3abf8d1d0decf6c

--- makeRSS_GoogleNews.py
+++ makeRSS_GoogleNews.py
@@ -1,11 +1,9 @@
-import feedparser
-import xml.etree.ElementTree as ET
-from xml.dom import minidom
-import os
+import requests
+from lxml import etree
 
 url_and_xmls = [
     {
-        'url': 'https://news.google.com/rss/articles/CBMiNGh0dHBzOi8vd3d3Lm5vZ2l6YWthNDYuY29tL3MvbjQ2L2RpYXJ5L2RldGFpbC8xMDE5ODHSAQA?oc=5',
+        'url': 'https://news.google.com/rss/topics/CAAqKAgKIiJDQkFTRXdvTkwyY3ZNVEZtZERCc2NIaDZNaElDYW1Fb0FBUAE?hl=ja&gl=JP&ceid=JP%3Aja',
         'xml': 'feed_GoogleNews_Yumiki.xml'
     }
 ]
@@ -17,41 +15,20 @@ def fetch_and_save_feeds(url_and_xmls):
 
         # RSSフィードを取得
         print(f'Fetching RSS feed from {url}')
-        feed = feedparser.parse(url)
-        print(f'Fetched {len(feed.entries)} entries.')
+        response = requests.get(url)
+        response.raise_for_status()  # ステータスコードが200 OKでない場合は、エラーを発生させる
         
-        # 既存のXMLファイルがあるか確認
-        try:
-            tree = ET.parse(xml_filename)
-            root = tree.getroot()
-        except FileNotFoundError:
-            # ファイルが存在しない場合、新しいXMLツリーを作成
-            root = ET.Element("rss")
-            tree = ET.ElementTree(root)
-
-        # 各エントリをXMLに追加
-        channel = root.find('channel')
-        if channel is None:
-            channel = ET.SubElement(root, 'channel')
-
-        for entry in feed.entries:
-            item = ET.SubElement(channel, 'item')
-            title = ET.SubElement(item, 'title')
-            title.text = entry.title
-            link = ET.SubElement(item, 'link')
-            link.text = entry.link
-            description = ET.SubElement(item, 'description')
-            description.text = entry.description
+        # RSSフィードの内容を解析
+        root = etree.fromstring(response.content)
+        entries = root.xpath('//item')
+        print(f'Fetched {len(entries)} entries.')
 
         # XMLファイルに保存
         print(f'Saving to {xml_filename}')
-        xml_str = ET.tostring(root, encoding='utf-8', method='xml')
-        dom = minidom.parseString(xml_str)
-        pretty_xml_str = dom.toprettyxml(indent='\t')
-        with open(xml_filename, 'w', encoding='utf-8') as f:
-            f.write(pretty_xml_str)
+        xml_str = etree.tostring(root, encoding='utf-8', pretty_print=True)
+        with open(xml_filename, 'wb') as f:
+            f.write(xml_str)
         print(f'Saved to {xml_filename}')
 
-# 実行
-if __name__ == "__main__":
-    fetch_and_save_feeds(url_and_xmls)
+# RSSフィードを取得して保存
+fetch_and_save_feeds(url_and_xmls)
