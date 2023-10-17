26b9b9a926f03f9ef17470ad81fafe5146baeee9
kh <127534515+kh5402@users.noreply.github.com>
2023-10-17 13:32:37 +0900

    Update makeRSS_GoogleNews.py

--- makeRSS_GoogleNews.py
+++ makeRSS_GoogleNews.py
@@ -1,6 +1,7 @@
 import os
 import requests
 from lxml import etree
+from datetime import datetime
 
 url_and_xmls = [
     {
@@ -27,13 +28,39 @@ def fetch_and_save_feeds(url_and_xmls):
         response.raise_for_status()  # ステータスコードが200 OKでない場合は、エラーを発生させる
         
         # RSSフィードの内容を解析
-        root = etree.fromstring(response.content)
-        entries = root.xpath('//item')
-        print(f'Fetched {len(entries)} entries.')
+        new_root = etree.fromstring(response.content)
+        new_entries = new_root.xpath('//item')
+
+        try:
+            with open(xml_filename, 'rb') as f:
+                existing_root = etree.parse(f).getroot()
+            existing_entries = existing_root.xpath('//item')
+        except FileNotFoundError:
+            existing_entries = []
+            existing_root = etree.Element("rss")
+            channel = etree.SubElement(existing_root, "channel")
+            channel.append(new_root.find('title'))
+            channel.append(new_root.find('link'))
+            channel.append(new_root.find('description'))
+
+        # 重複を避けて、新しいエントリーを追加
+        existing_guids = {entry.find('guid').text for entry in existing_entries}
+        for entry in new_entries:
+            if entry.find('guid').text not in existing_guids:
+                existing_root.find('channel').append(entry)
+
+        # 日付でソート
+        existing_entries = existing_root.xpath('//item')
+        existing_entries.sort(key=lambda e: datetime.strptime(e.find('pubDate').text, '%a, %d %b %Y %H:%M:%S %Z'), reverse=True)
+        
+        # ソートされたエントリーを再配置
+        channel = existing_root.find('channel')
+        for entry in existing_entries:
+            channel.append(entry)
 
         # XMLファイルに保存
         print(f'Saving to {xml_filename}')
-        xml_str = etree.tostring(root, encoding='utf-8', pretty_print=True)
+        xml_str = etree.tostring(existing_root, encoding='utf-8', pretty_print=True)
         with open(xml_filename, 'wb') as f:
             f.write(xml_str)
         print(f'Saved to {xml_filename}')
