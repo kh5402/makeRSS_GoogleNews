d1ab363d363203844ea209e178fa75976e2e4e5b
kh <127534515+kh5402@users.noreply.github.com>
2023-10-17 12:14:03 +0900

    Update makeRSS_GoogleNews.py

--- makeRSS_GoogleNews.py
+++ makeRSS_GoogleNews.py
@@ -1,7 +1,5 @@
-import feedparser
+import requests
 import xml.etree.ElementTree as ET
-from xml.dom import minidom
-import os
 
 url_and_xmls = [
     {
@@ -17,47 +15,20 @@ def fetch_and_save_feeds(url_and_xmls):
 
         # RSSフィードを取得
         print(f'Fetching RSS feed from {url}')
-        feed = feedparser.parse(url)
-        
-        # エラーのチェック
-        if feed.bozo:
-            print(f'Error: {feed.bozo_exception}')
-            continue  # エラーがあれば、このフィードの処理をスキップ
+        response = requests.get(url)
+        response.raise_for_status()  # ステータスコードが200 OKでない場合は、エラーを発生させる
 
-        print(f'Fetched {len(feed.entries)} entries.')
-        
-        # 既存のXMLファイルがあるか確認
-        try:
-            tree = ET.parse(xml_filename)
-            root = tree.getroot()
-        except FileNotFoundError:
-            # ファイルが存在しない場合、新しいXMLツリーを作成
-            root = ET.Element("rss")
-            tree = ET.ElementTree(root)
-
-        # 各エントリをXMLに追加
-        channel = root.find('channel')
-        if channel is None:
-            channel = ET.SubElement(root, 'channel')
-
-        for entry in feed.entries:
-            item = ET.SubElement(channel, 'item')
-            title = ET.SubElement(item, 'title')
-            title.text = entry.title
-            link = ET.SubElement(item, 'link')
-            link.text = entry.link
-            description = ET.SubElement(item, 'description')
-            description.text = entry.description
+        # RSSフィードの内容を解析
+        root = ET.fromstring(response.content)
+        entries = root.findall('.//item')
+        print(f'Fetched {len(entries)} entries.')
 
         # XMLファイルに保存
         print(f'Saving to {xml_filename}')
         xml_str = ET.tostring(root, encoding='utf-8', method='xml')
-        dom = minidom.parseString(xml_str)
-        pretty_xml_str = dom.toprettyxml(indent='\t')
-        with open(xml_filename, 'w', encoding='utf-8') as f:
-            f.write(pretty_xml_str)
+        with open(xml_filename, 'wb') as f:
+            f.write(xml_str)
         print(f'Saved to {xml_filename}')
 
-# 実行
-if __name__ == "__main__":
-    fetch_and_save_feeds(url_and_xmls)
+# RSSフィードを取得して保存
+fetch_and_save_feeds(url_and_xmls)
